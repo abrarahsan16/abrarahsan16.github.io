c775fb6c2e6909f2cb9a5463bcb933647a0e346f
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2021-09-06 22:57:18 -0400

    Starting the about me

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -51720,8 +51720,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 const Home = () => {
   return (
     /*#__PURE__*/
-    (0, _jsxRuntime.jsx)("section", {
-      children:
+    (0, _jsxRuntime.jsxs)("section", {
+      children: [
       /*#__PURE__*/
       (0, _jsxRuntime.jsx)(_reactBootstrap.Container, {
         fluid: true,
@@ -51789,7 +51789,19 @@ const Home = () => {
             })]
           })
         })
-      })
+      }),
+      /*#__PURE__*/
+      (0, _jsxRuntime.jsx)("svg", {
+        xmlns: "http://www.w3.org/2000/svg",
+        viewBox: "0 0 1440 320",
+        children:
+        /*#__PURE__*/
+        (0, _jsxRuntime.jsx)("path", {
+          fill: "#386fa4",
+          "fill-opacity": "1",
+          d: "M0,32L48,32C96,32,192,32,288,32C384,32,480,32,576,42.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+        })
+      })]
     })
   );
 };
