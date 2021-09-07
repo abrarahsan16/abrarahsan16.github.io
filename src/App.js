082860b51254edab24d2aa21e6dd9c22d385676b
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2021-09-06 21:23:54 -0400

    Set up initial bits

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { render } from "react-dom";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Experience from "./Components/Experience";
 import Projects from "./Components/Projects";
+import Resume from "./Components/Resume";
+import Footer from "./Components/Footer";
+import "./style.css";
+import "./App.css";
 
-export default function App() {
+const App = () => {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <About />
-      <Experience />
-      <Projects />
-    </main>
+    <Home />
+    /*<Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/project" component={Projects} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+    */
   );
-}
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/Components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 className="heading">Hi there!</h1>
+              <h1 className="heading-name">
+                My name is
+                <strong className="main-name">Abrar Ahsan!</strong>
+              </h1>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
+};
+
+export default Home;
